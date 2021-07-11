5e69aab17fda7ae4813814121c4e5820f1c58bfd
Catherine Matta <catherine.matta22399@gmail.com>
2021-07-11 18:08:10 -0400

    Initial Commit, Added Flutter Folder

---
+++ client/lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+// Those are the 3 main colors of the theme
+const BackgroundColor = Color.fromRGBO(242, 252, 252, 1);
+const MedGreen = Color.fromRGBO(70, 101, 81, 1);
+const LightGreen = Color.fromRGBO(54, 140, 114, 1);
+
+const Black = Color.fromRGBO(00, 00, 00, 1);
