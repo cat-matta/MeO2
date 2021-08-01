f06e54d8b759ebe7f5649575e74c22cba0cf51f4
Catherine Matta <catherine.matta22399@gmail.com>
2021-08-01 19:09:54 -0400

    created api to retrieve info from db

--- server/api.py
+++ server/api.py
@@ -1,25 +1,25 @@
-from flask import Flask
-from flask_cors import CORS
+from flask import Flask, jsonify
 import pymongo
 import dns
 import os
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv()
-
-Mongo_URI = os.getenv('apiuri')
+Mongo_URI = os.getenv('api')
 client = pymongo.MongoClient(Mongo_URI)
-
-# client = pymongo.MongoClient('mongodb+srv://<NAME>:<PASS>@cluster0.bxtpa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-
-data = client.get_database("")## Access our collection
-
+db = client.get_database("Env")
 
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/<something>")
+def get_carbon(something):
 
-@app.route("/<item>")
-def helloWorld():
-  return data[item]
+	want=db['Info'].find_one({'name':something}) # to locate the entry
 
+	return jsonify(want['carbon']) # get name at said entry
+
+
+if __name__ == "__main__":
+	app.run()
